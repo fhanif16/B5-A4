caf70b2083ef6710fa0d0cec9619ffdfc846ccb9
fhanif16 <fatemahanif16@gmail.com>
2025-07-03 20:40:33 -0400

    md file

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: 'baseApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://b5-a3.vercel.app/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://b5-a3.vercel.app' }),
   tagTypes: ['Books'],
   endpoints: (build) => ({
     getBooks: build.query({
